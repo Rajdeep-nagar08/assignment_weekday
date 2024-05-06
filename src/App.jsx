a2f766aff1cea31253a3d981ed54cc9a0d35625d
Rajdeep Nagar <rajdeepnagar08@gmail.com>
2024-05-07 01:05:09 +0530

    Fixing background : removing gridlines from background

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,7 @@ import GridLines from "react-gridlines";
 
 function App() {
   return (
-    <GridLines
-      className="bg-slate-100"
-      lineColor="#fcfcfc"
-      cellWidth={50}
-      strokeWidth={2}
-      cellWidth2={50}
-    >
       <Home />
-      </GridLines>
   );
 }
 
